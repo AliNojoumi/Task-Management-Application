12c747f0c636ae0994a3129ddc0865c75149c451
alinojomi <anojomi3@gmail.com>
2024-03-06 10:29:32 +0330

    Added createPost to the /tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: Math.floor(Math.random() * 1000).toString(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
