f09d1836ad42f2c29fa064aee90fd7c974c7d0ab
alinojomi <anojomi3@gmail.com>
2024-03-07 21:41:20 +0330

    Added class-validator to the CreateTaskDto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,8 +1,16 @@
+import { IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
+
+  @IsNotEmpty()
   finishedBy: string;
+
+  @IsNotEmpty()
   status: TaskStatus;
 }
