43ed2c21a77f5a39ae9f2b8e5f34f038c0021d9f
alinojomi <anojomi3@gmail.com>
2024-03-07 21:45:59 +0330

    Added class-validator to the CreateUserDto

--- src/users/user-dto/create-user.dto.ts
+++ src/users/user-dto/create-user.dto.ts
@@ -1,8 +1,21 @@
 import { UserStatus } from '../user.model';
+import { IsNotEmpty, IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   status: UserStatus;
 }
